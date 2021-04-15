b739522a3e7065f47aa1f6b3bbc28bb272afdd66
gabrielemoia <gabriele.moia@lynxspa.com>
2021-04-15 22:04:47 +0200

    Fix method name

--- www/AddressBookPlugin.js
+++ www/AddressBookPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+exports.saveContact = function (arg0, success, error) {
     exec(success, error, 'AddressBookPlugin', 'saveContact', [arg0]);
 };
