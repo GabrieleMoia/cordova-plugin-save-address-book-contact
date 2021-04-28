52c79a18e0427a8eef9b77538df2a1efc51607c8
gabrielemoia <gabriele.moia@lynxspa.com>
2021-04-28 09:26:18 +0200

    Added request access

--- src/ios/AddressBookPlugin.swift
+++ src/ios/AddressBookPlugin.swift
@@ -1,39 +1,43 @@
 import Contacts
 import ContactsUI
 
-@objc(AddressBookPlugin) class AddressBookPlugin : CDVPlugin, CNContactViewControllerDelegate , UIViewControllerTransitioningDelegate{
+@objc(AddressBookPlugin) class AddressBookPlugin : CDVPlugin, CNContactViewControllerDelegate , UIViewControllerTransitioningDelegate {
     // MARK: Properties
     var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    let store = CNContactStore()
+    
     @objc(saveContact:) func saveContact(_ command: CDVInvokedUrlCommand) {
-        CNContactStore().requestAccess(for: .contacts) { (access, error) in
-            print("Access: \(access)")
-        }
-        
         let contact = CNMutableContact()
         
-        if let value = command.arguments[0] as? [String: Any] {
-            if let name = value["NOME"] as? String, let secondName = value["COGNOME"] as? String {
-                contact.givenName = name
-                contact.familyName = secondName
-                if let number = value["NUMERO_CELL"] as? String {
-                    contact.phoneNumbers = [CNLabeledValue(
-                                                label:CNLabelPhoneNumberiPhone,
-                                                value:CNPhoneNumber(stringValue: number))]
-                }
-                if let email = value["MAIL_UTENTE"] as? String {
-                    let workEmail = CNLabeledValue(label:CNLabelWork, value: email as NSString)
-                    contact.emailAddresses = [workEmail]
+        self.requestAccess { (value) in
+            if (value) {
+                if let value = command.arguments[0] as? [String: Any] {
+                    if let name = value["NOME"] as? String, let secondName = value["COGNOME"] as? String {
+                        contact.givenName = name
+                        contact.familyName = secondName
+                        if let number = value["NUMERO_CELL"] as? String {
+                            contact.phoneNumbers = [CNLabeledValue(
+                                                        label:CNLabelPhoneNumberiPhone,
+                                                        value:CNPhoneNumber(stringValue: number))]
+                        }
+                        if let email = value["MAIL_UTENTE"] as? String {
+                            let workEmail = CNLabeledValue(label:CNLabelWork, value: email as NSString)
+                            contact.emailAddresses = [workEmail]
+                        }
+                    }
+                    
+                    let controller = CNContactViewController(forUnknownContact: contact)
+                    controller.contactStore = self.store
+                    controller.delegate = self
+                    controller.allowsActions = true
+                    controller.title = "Aggiungi Contatto"
+                    let navigationController = UINavigationController(rootViewController: controller)
+                    navigationController.setNavigationBarHidden(false, animated: true)
+                    self.viewController.present(navigationController, animated: true, completion: nil)
                 }
             }
-            let store = CNContactStore()
-            let controller = CNContactViewController(forUnknownContact: contact)
-            controller.contactStore = store
-            controller.delegate = self
-            controller.allowsActions = true
-            controller.title = "Aggiungi Contatto"
-            let navigationController = UINavigationController(rootViewController: controller)
-            self.viewController.present(navigationController, animated: true, completion: nil)
         }
+        
     }
     
     func contactViewController(_ viewController: CNContactViewController, didCompleteWith contact: CNContact?) {
@@ -43,5 +47,40 @@ import ContactsUI
     func contactViewController(_ viewController: CNContactViewController, shouldPerformDefaultActionFor property: CNContactProperty) -> Bool {
         print(property.key)
         return true
+    }
+    
+    func requestAccess(completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        switch CNContactStore.authorizationStatus(for: .contacts) {
+        case .authorized:
+            completionHandler(true)
+        case .denied:
+            self.showSettingsAlert(completionHandler)
+        case .restricted, .notDetermined:
+            self.store.requestAccess(for: .contacts) { granted, error in
+                if granted {
+                    completionHandler(true)
+                } else {
+                    DispatchQueue.main.async {
+                        self.showSettingsAlert(completionHandler)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func showSettingsAlert(_ completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
+        let alert = UIAlertController(title: nil, message: "Per salvare il contatto in rubrica, è necessario abilitare Impostazioni > People Directory > Contatti", preferredStyle: .alert)
+        if let url = URL(string:UIApplicationOpenSettingsURLString) {
+            if UIApplication.shared.canOpenURL(url) {
+                alert.addAction(UIAlertAction(title: "Apri impostazioni", style: .default) { action in
+                    completionHandler(false)
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                })
+            }
+        }
+        alert.addAction(UIAlertAction(title: "Annulla", style: .cancel) { action in
+            completionHandler(false)
+        })
+        self.viewController.present(alert, animated: true, completion: nil)
     }
 }
